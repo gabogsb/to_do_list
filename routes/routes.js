08d00978474814a773129d94eefcaaa650189d19
gabogsb <gbarcellos1701@gmail.com>
2022-09-09 03:45:15 -0300

    update, delete

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,10 @@ const routes = require("express").Router();
 const TaskController = require("../controller/TaskController");
 
 routes.get("/", TaskController.getAllTasks);
+routes.get("/getByID/:id/:method", TaskController.getById);
+routes.get("/deleteOne/:id", TaskController.deleteOneTask);
+
 routes.post("/create", TaskController.createTask);
+routes.post("/updateOne/:id", TaskController.updateOneTask);
 
 module.exports = routes;
